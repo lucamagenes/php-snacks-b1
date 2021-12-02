bd532c3ca11c0fda4b3f918d4b4ddf8df49271c3
lucamagenes <lucamgns33@gmail.com>
2021-12-02 15:26:20 +0100

    commit snack 3

---
+++ snack-3/index.php
@@ -0,0 +1,56 @@
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+//var_dump($posts, $posts['10/01/2019']);
+
+for($i=0; $i < count($posts); $i++) {
+    $element = $posts[$i];
+    var_dump($element);
+
+    /* for($index=0; $index < count($element); $index++) {
+        $post = $element[$index];
+        var_dump($post, $post['title'], $post['author'], $post['text']);
+    } */
+}
+
+
+?>
