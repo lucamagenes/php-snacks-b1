08223ef120bb932e4f394628aad52fde84c47eec
lucamagenes <lucamgns33@gmail.com>
2021-12-02 15:59:40 +0100

    snack 3 complete

--- snack-3/index.php
+++ snack-3/index.php
@@ -1,5 +1,17 @@
 <?php
 
+/* 
+
+Snack 3
+
+Creare un array di array. 
+Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post.
+
+*/
+
+
 $posts = [
 
     '10/01/2019' => [
@@ -40,17 +52,55 @@ $posts = [
     ],
 ];
 
-//var_dump($posts, $posts['10/01/2019']);
 
-for($i=0; $i < count($posts); $i++) {
-    $element = $posts[$i];
-    var_dump($element);
 
-    /* for($index=0; $index < count($element); $index++) {
-        $post = $element[$index];
-        var_dump($post, $post['title'], $post['author'], $post['text']);
-    } */
-}
+/* foreach ($posts as $data_post => $post) {
+    var_dump($data_post, $post);
+    foreach($post as $info => $details) {
+        echo  $details['title'] . $details['author'] . $details['text'];
+    }
+} */
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Posts</h1>
+    
+    <?php foreach ($posts as $data_post => $post) { ?>
+        <p>
+            Data del post: 
+            <?php echo $data_post ?>
+        </p> 
+
+        <?php foreach($post as $info => $details) { ?>
+
+            <ul>
+                <li>
+                    Titolo: 
+                    <?php echo  $details['title'] ?>
+                </li>
+                <li>
+                    Autore: 
+                    <?php echo  $details['author'] ?>
+                </li>
+                <li>
+                    Testo: 
+                    <?php echo  $details['text'] ?>
+                </li>
+            </ul>
+        
+        <?php } ?>
+    
+    <?php } ?>
+
+</body>
+</html>
